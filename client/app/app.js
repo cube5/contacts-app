d546aa06ce36e400b54fe99f858021d8fd8e9e6c
Daniel sanchez <daniel.sanchez@paralife.com.mx>
2016-02-10 05:13:04 +0000

    :zap: adding angular-material

--- client/app/app.js
+++ client/app/app.js
@@ -1,22 +1,30 @@
 
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+import ngMaterial from 'angular-material';
 import angularComponent from 'angular-component';
 import Common from './common/common';
 import Components from './components/components';
 import AppComponent from './app.component';
+
 import 'normalize.css';
+import 'angular-material/angular-material.css';
 import 'foundation-sites/dist/foundation.min.css';
 
 console.log('app module')
 angular.module('app', [
   uiRouter,
+  ngMaterial,
   Common.name,
   Components.name
 ])
-.config(($urlRouterProvider) => {
-    $urlRouterProvider.otherwise('/');
-  })
+.config(($urlRouterProvider, $mdThemingProvider) => {
+  $urlRouterProvider.otherwise('/');
+  
+  $mdThemingProvider.theme('default')
+    .primaryPalette('blue')
+    .accentPalette('pink');
+})
 .component('app', AppComponent);
 
 angular.element(document).ready(() => {
